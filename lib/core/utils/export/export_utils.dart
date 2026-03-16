3b5fc37ad1bce31a6433123682921e2a2859d2ac
carlos.guillem <147244081+carloscgm@users.noreply.github.com>
2026-03-16 10:48:21 +0100

    migrate to lib export

--- lib/core/utils/export/export_utils.dart
+++ lib/core/utils/export/export_utils.dart
@@ -1,9 +1,11 @@
 // Facade that conditionally imports the correct implementation for the current platform.
 import 'dart:typed_data';
 
-import 'export_utils_io.dart'
-    if (dart.library.html) 'export_utils_web.dart' as impl;
+import 'package:file_saver/file_saver.dart';
 
-Future<String> downloadFile(String filename, Uint8List bytes, String mime) async {
-  return await impl.downloadFile(filename, bytes, mime);
-}
+Future<String> downloadFile(String filename, Uint8List bytes, String mime) =>
+    FileSaver.instance.saveFile(
+      name: filename,
+      bytes: bytes,
+      customMimeType: mime,
+    );
